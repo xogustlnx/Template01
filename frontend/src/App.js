69cb94bb2c45c153c387a10bdd70cd2f6a560ea5
Triangleyj <dpwns99@gmail.com>
2021-08-21 04:41:42 +0900

    Add simple input

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [message, setMessage] = useState('');
+  const handleChange = e => setMessage(e.target.value);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="App-header">
+        <label>
+          <input className="App-input" type="text" placeholder="Enter any message" value={message} onChange={handleChange} />
+        </label>
+        <input className="App-submit" type="submit" value="Save"/>
+      </form>
     </div>
   );
 }
