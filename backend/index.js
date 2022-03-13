dee708f05c182e31b9387852d4644dd3e806a182
Sin taehyeon <97009497+xogustlnx@users.noreply.github.com>
2022-03-14 01:09:41 +0900

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.PORT;
+const port = 8080;
 const api = require('./routes/my_api');
 require('./utils/mongo');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.status(419).json({ok: true, me: 'teapot'})
